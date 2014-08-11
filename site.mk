c76a9556ac884341139c366b0ff32d8a0c2fece6
Christof Schulze <christof.schulze@gmx.net>
2014-08-11 22:03:48 +0200

    add private wifi config to expert mode

--- site.mk
+++ site.mk
@@ -14,6 +14,7 @@ GLUON_SITE_PACKAGES := \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
 	gluon-luci-portconfig \
+	gluon-luci-private-wifi \
 	gluon-next-node \
 	gluon-mesh-vpn-fastd \
 	gluon-ffmautokey \
