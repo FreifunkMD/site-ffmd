35ac5109770195ae1cb311d7a8d5179b62d71c26
Alexander Dahl <post@lespocky.de>
2014-09-21 15:54:50 +0200

    activate private WLAN, fixes #15

--- site.mk
+++ site.mk
@@ -21,6 +21,7 @@ GLUON_SITE_PACKAGES := \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
 	gluon-luci-portconfig \
+	gluon-luci-private-wifi \
 	gluon-next-node \
 	gluon-mesh-vpn-fastd \
 	gluon-radvd \
