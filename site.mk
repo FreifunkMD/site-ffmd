12dfeace29eaba5b9c08a051e4b6592d35b00a36
Alexander Dahl <alex@netz39.de>
2015-10-29 12:42:06 +0100

    site changes for gluon 2015.2.x

--- site.mk
+++ site.mk
@@ -33,10 +33,16 @@ GLUON_SITE_PACKAGES := \
 	iptables \
 	iwinfo
 
-DEFAULT_GLUON_CHECKOUT := v2015.1.2
+DEFAULT_GLUON_CHECKOUT := master
 
 # Allow overriding the checkout from the command line
 GLUON_CHECKOUT ?= $(DEFAULT_GLUON_CHECKOUT)
+
+##	DEFAULT_GLUON_RELEASE
+#		version string to use for images
+#		gluon relies on
+#			opkg compare-versions "$1" '>>' "$2"
+#		to decide if a version is newer or not.
 
 # replace magic in version number as follows:
 # v0.31-4-gf390c9d				--> 0.31+4-gf390c9d
@@ -49,6 +55,13 @@ DEFAULT_GLUON_RELEASE := $(shell git --git-dir=$(this_dir)/.git \
 		| sed -e 's/^\(v[^-]\+\)-\(beta.*\)$$/\1~\2/' \
 		| sed -e 's/^\(v[^~]\+~beta[^-]\+\)-\([0-9]\+-g[0-9a-f]\{7\}.*\)$$/\1+\2/' \
 		| sed -e 's/^v//')
+
+##	GLUON_RELEASE
+#		call make with custom GLUON_RELEASE flag, to use your own release version scheme.
+#		e.g.:
+#			$ make images GLUON_RELEASE=23.42+5
+#		would generate images named like this:
+#			gluon-ff%site_code%-23.42+5-%router_model%.bin
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
