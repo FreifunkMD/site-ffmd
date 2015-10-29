540d54249e677b77d397a76e04476c6dd2ba6f8e
JuS <sagurna@sagutech.de>
2015-10-29 16:23:56 +0100

    Anpassungen Struktur von site.conf und site.mk für Gluon v2015.2 Release

--- site.mk
+++ site.mk
@@ -18,13 +18,14 @@ GLUON_SITE_PACKAGES := \
 	gluon-luci-private-wifi \
 	gluon-luci-mesh-vpn-fastd \
 	gluon-luci-wifi-config \
+	gluon-luci-node-role \
+	gluon-luci-switchconfig \
 	gluon-next-node \
 	gluon-mesh-vpn-fastd \
 	gluon-radvd \
 	gluon-status-page \
 	iwinfo \
 	iptables \
-	gluon-luci-switchconfig \
 	ffffm-ebtables-net-rules \
 	haveged 
 #	gluon-ffmcollectdconfig \
@@ -38,10 +39,34 @@ GLUON_SITE_PACKAGES := \
 #	collectd-mod-memory \
 #	gluon-ffmautokey \
 
-DEFAULT_GLUON_RELEASE := 2015.1.2-$(shell date '+%Y%m%d')
+
+
+#####################################################################################################################
+# Firmware Versioning
+#####################################################################################################################
+# Release Stable: x.y-stable
+# Release Test:   x.y.z-test
+# Release Dev:    x.y.z.w-dev (+Date/Time)
+#
+# Geht eine Dev-Version in Test über, so können identische  Dateien in 'test' und 'dev' abgelegt werden.
+# Geht eine Test-Version in Stable über, so können identische Dateien in 'stable', 'test' und 'dev' abgelegt werden. 
+#
+# Autoupdate ist weiterhin gewehrleistet! 
+# Per Definition ist 'x.y-stable' grösser als 'x.y.z-test' und grösser als 'x.y.z.w-dev'
+#####################################################################################################################
+
+# This is the dev branch -> x.y.z.w-dev
+DEFAULT_GLUON_RELEASE := ffm-1.10.0.2-dev        # This needs to be changed after each online release
+
+# For homebrew development add e.g. date and time 
+# (don't use the ':' char. It will break the build)
+# DEFAULT_GLUON_RELEASE := $(DEFAULT_GLUON_RELEASE)-$(shell date '+%m.%d-%H%M')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+
+# Development branch information
+GLUON_BRANCH ?= dev
 
 # Default priority for updates.
 # GLUON_PRIORITY ?= 0
