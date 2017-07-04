2931bb01c1be4fa868299c6061936b9fe241f2a4
Vitapower87 <robert-schuette@web.de>
2017-07-04 19:41:06 +0200

    Add files via upload

--- site.mk
+++ site.mk
@@ -1,14 +1,6 @@
-# http://stackoverflow.com/questions/18136918/how-to-get-current-directory-of-your-makefile
-this_dir := $(shell dirname $(abspath $(lastword $(MAKEFILE_LIST))))
-
-##	GLUON_SITE_PACKAGES
-#		specify gluon/openwrt packages to include here
-#		The gluon-mesh-batman-adv-* package must come first because of the dependency resolution
-
 GLUON_SITE_PACKAGES := \
 	gluon-mesh-batman-adv-14 \
 	gluon-alfred \
-	gluon-respondd \
 	gluon-autoupdater \
 	gluon-config-mode-autoupdater \
 	gluon-config-mode-contact-info \
@@ -18,58 +10,29 @@ GLUON_SITE_PACKAGES := \
 	gluon-config-mode-mesh-vpn \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
-	gluon-luci-admin \
-	gluon-luci-autoupdater \
-	gluon-luci-portconfig \
-	gluon-luci-private-wifi \
-	gluon-luci-wifi-config \
-	gluon-next-node \
-	gluon-neighbour-info \
+	gluon-ebtables-segment-mld \
+	gluon-web-admin \
+	gluon-web-autoupdater \
+	gluon-web-mesh-vpn-fastd \
+	gluon-web-network \
+	gluon-web-wifi-config \
+	gluon-web-private-wifi \
 	gluon-mesh-vpn-fastd \
 	gluon-radvd \
+	gluon-respondd \
 	gluon-setup-mode \
 	gluon-status-page \
-	haveged \
-	iptables \
-	iwinfo
+	iwinfo \
+	haveged
 
-DEFAULT_GLUON_CHECKOUT := v2016.2.4
 
-# Allow overriding the checkout from the command line
-GLUON_CHECKOUT ?= $(DEFAULT_GLUON_CHECKOUT)
-
-##	DEFAULT_GLUON_RELEASE
-#		version string to use for images
-#		gluon relies on
-#			opkg compare-versions "$1" '>>' "$2"
-#		to decide if a version is newer or not.
-
-# replace magic in version number as follows:
-# v0.31-4-gf390c9d				--> 0.31+4-gf390c9d
-# v0.32-beta.1 					--> 0.32~beta.1
-# v0.30-beta.1-2-g11c8a08-dirty	--> 0.30~beta.1+2-g11c8a08-dirty
-DEFAULT_GLUON_RELEASE := $(shell git --git-dir=$(this_dir)/.git \
-		--work-tree=$(this_dir) describe --tags --always --dirty \
-		--match "v*" \
-		| sed -e 's/^\(v[^-]\+\)-\([0-9]\+-g[0-9a-f]\{7\}.*\)$$/\1+\2/' \
-		| sed -e 's/^\(v[^-]\+\)-\(beta.*\)$$/\1~\2/' \
-		| sed -e 's/^\(v[^~]\+~beta[^-]\+\)-\([0-9]\+-g[0-9a-f]\{7\}.*\)$$/\1+\2/' \
-		| sed -e 's/^v//')
-
-##	GLUON_RELEASE
-#		call make with custom GLUON_RELEASE flag, to use your own release version scheme.
-#		e.g.:
-#			$ make images GLUON_RELEASE=23.42+5
-#		would generate images named like this:
-#			gluon-ff%site_code%-23.42+5-%router_model%.bin
+DEFAULT_GLUON_RELEASE := v2017.1.1~exp$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
-# Default priority for updates.
 GLUON_PRIORITY ?= 0
 
-# Languages to include
 GLUON_REGION ?= eu
 GLUON_LANGS ?= en de
 
