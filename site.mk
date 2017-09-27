80444305e2e8ba81d51b0156aafdac12c4796f7d
Christof Schulze <christof.schulze@gmx.net>
2017-09-27 23:33:08 +0200

    include ffffm-banner

--- site.mk
+++ site.mk
@@ -23,12 +23,12 @@ GLUON_SITE_PACKAGES := \
 	gluon-config-mode-mesh-vpn \
 	gluon-web-mesh-vpn-fastd \
 	gluon-status-page \
+	ffffm-banner \
 	prefixd
 
 # lede-wechsel, da ist paketumbau erforderlich und die folgenden Pakete haben diesen Umbau noch nicht erfahren:
 #ffffm-keep-radio-channel \
 #	ffffm-autoupdater-use-site-conf-branch \
-#	ffffm-banner \
 #	ffffm-additional-wifi-json-info \
 #
 
