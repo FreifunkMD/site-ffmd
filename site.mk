d4655d21acb3290c1550defa2cc8d65f3ca6a258
Vitapower87 <robert-schuette@web.de>
2017-10-26 08:18:15 +0200

    update gluon 2017.1.3

--- site.mk
+++ site.mk
@@ -26,7 +26,7 @@ GLUON_SITE_PACKAGES := \
 	haveged
 
 
-DEFAULT_GLUON_RELEASE := v2017.1.1~exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := v2017.1.3~exp$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
