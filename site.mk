82564e8d8aac9a61ec11182f5416e1b780001e9b
Christof Schulze <christof.schulze@gmx.net>
2018-01-01 15:11:00 +0100

    site.mk: remove prefixd in an attempt to resolve build issues due to iproute2

--- site.mk
+++ site.mk
@@ -25,8 +25,7 @@ GLUON_SITE_PACKAGES := \
 	gluon-status-page \
 	ffffm-banner \
 	ffffm-keep-radio-channel \
-	gluon-iptables-clamp-mss-to-pmtu \
-	prefixd
+	gluon-iptables-clamp-mss-to-pmtu
 
 # ffffm-autoupdater-use-site-conf-branch \
 
